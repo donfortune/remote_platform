891d6d4b704387c7d70703d2dadba9a3bd10f186
donfortune <donfortunet.df@gmail.com>
2024-10-02 09:31:09 +0100

    created a signal to create and save a new profile after creating a new user

--- job_platform/remote/signals.py
+++ job_platform/remote/signals.py
@@ -3,11 +3,13 @@ from django.dispatch import receiver
 from django.contrib.auth.models import User
 from .models import Profile
 
+#create a profile for every new user
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
         Profile.objects.create(user=instance)
 
+#save the profile for every new user
 @receiver(post_save, sender=User)
 def save_user_profile(sender, instance, **kwargs):
     instance.profile.save()
