821458729d97853aba2d5dbf2df569fdb0a63b91
donfortune <donfortunet.df@gmail.com>
2024-10-03 09:27:17 +0100

    handle seperrate dashboard for recruiter and job seeker

--- job_platform/remote/urls.py
+++ job_platform/remote/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('login/', views.login_view, name='login'),
     path('signup/', views.signup_view, name='signup'),
+    path('signup_recruiter/', views.signup_recruiter_view, name='signup_recruiter'),
     path('jobs_dashboard/', views.jobs_dashboard, name='jobs_dashboard'),
     path('logout/', views.logout_view, name='logout'),
     path('recruiter_dashboard/', views.recruiter_dashboard, name='recruiter_dashboard'),
--- job_platform/remote/views.py
+++ job_platform/remote/views.py
@@ -34,18 +34,41 @@ def signup_view(request):
                 company_name=form.cleaned_data.get('company_name', ''),
                 resume=form.cleaned_data.get('resume', None),
             )
-            login(request, user)  # Automatically log in the user after signup
+            login(request, user)
+            
+              
 
             # Check the role and redirect to the appropriate dashboard
             if profile.role == 'job_seeker':
                 return redirect('jobs_dashboard')  # URL name for the job seeker dashboard
             elif profile.role == 'recruiter':
                 return redirect('recruiter_dashboard')  # URL name for the recruiter dashboard
+             
 
     else:
         form = CustomUserCreationForm()
 
     return render(request, 'signup.html', {'form': form})
+
+def signup_recruiter_view(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST, request.FILES)
+        if form.is_valid():
+            user = form.save()
+            profile = Profile.objects.create(
+                user=user,
+                role=form.cleaned_data['role'],
+                bio=form.cleaned_data.get('bio', ''),
+                location=form.cleaned_data.get('location', ''),
+                company_name=form.cleaned_data.get('company_name', ''),
+                resume=form.cleaned_data.get('resume', None),
+            )
+            login(request, user)
+            return redirect('recruiter_dashboard')
+    else:
+        form = CustomUserCreationForm()
+
+    return render(request, 'signup_recruiter.html', {'form': form})
 
 def login_view(request):
     if request.method == 'POST':
@@ -58,12 +81,25 @@ def login_view(request):
         form = AuthenticationForm()
     return render(request, 'login.html', {'form': form})
 
+def login_recruiter_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return render(request, 'recruiters.html')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'login_recruiter.html', {'form': form})
+
 def logout_view(request):
     logout(request)
     return render(request, 'index.html')
 
 def recruiter_dashboard(request):
     return render(request, 'recruiters.html')
+
+
 
 @login_required
 def jobs_dashboard(request):
